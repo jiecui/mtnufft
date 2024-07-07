e878b712f68ff7f39ded4aefdc99ddfcf01fa461
Richard Jie Cui <richard.cui@utoronto.ca>
2024-07-07 00:58:03 -0500

    chore: Add plot_error_analysis function for error analysis

---
+++ functions/plot_error_analysis.m
@@ -0,0 +1,20 @@
+function plot_error_analysis
+    % PLOT_ERROR_ANALYSIS (summary)
+    %
+    % Syntax:
+    %
+    % Input(s):
+    %
+    % Output(s):
+    %
+    % Example:
+    %
+    % Note:
+    %
+    % References:
+    %
+    % See also .
+
+    % Copyright 2024 Richard J. Cui. Created: Sun 07/07/2024 00:53:19.205 AM
+    % $Revision: 0.1 $  $Date: Sun 07/07/2024 00:53:19.206 AM $
+    %
