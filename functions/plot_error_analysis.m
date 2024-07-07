598af1adac4514af880b6c438b94a1ebcabeedf6
Richard Jie Cui <richard.cui@utoronto.ca>
2024-07-07 01:03:55 -0500

    chore: Update plot_error_analysis function with contact information

--- functions/plot_error_analysis.m
+++ functions/plot_error_analysis.m
@@ -15,6 +15,14 @@ function plot_error_analysis
     %
     % See also .
 
-    % Copyright 2024 Richard J. Cui. Created: Sun 07/07/2024 00:53:19.205 AM
-    % $Revision: 0.1 $  $Date: Sun 07/07/2024 00:53:19.206 AM $
+    % Copyright 2024 Richard J. Cui. Created: Sun 07/07/2024 01:03:08.462 AM
+    % $Revision: 0.1 $  $Date: Sun 07/07/2024 01:03:08.463 AM $
     %
+    % Rocky Creek Dr. NE
+    % Rochester, MN 55906, USA
+    %
+    % Email: richard.cui@utoronto.ca
+
+end % function plot_error_analysis
+
+% [EOF]
